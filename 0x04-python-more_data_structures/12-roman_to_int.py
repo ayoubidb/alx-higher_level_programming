9354f27e73bdad4f64472a3093decb12c549bb08
ayoubidb <ayoubidb@gmail.com>
2023-12-06 11:37:15 -0800

    done

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-def convert_roman(ch):
-    """
-    converts a roman numeral character into the respective integer
-    """
-    ret = -1
-    if ch == 'I':
-        ret = 1
-    elif ch == 'V':
-        ret = 5
-    elif ch == 'X':
-        ret = 10
-    elif ch == 'L':
-        ret = 50
-    elif ch == 'C':
-        ret = 100
-    elif ch == 'D':
-        ret = 500
-    elif ch == 'M':
-        ret = 1000
-    return ret
+def roman_to_int(roman_string):
+    """ converts a Roman numeral to an integer."""
+    romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    if (roman_string is None) or (type(roman_string) is not str):
+        return 0
+
+    number = len(roman_string)
+    value_int = romans[roman_string[number-1]]
+    for i in range(number - 1, 0, -1):
+        current_value = romans[roman_string[i]]
+        previous_value = romans[roman_string[i-1]]
+
+        if previous_value >= current_value:
+            value_int += previous_value
+        else:
+            value_int -= previous_value
+
+    return value_int
